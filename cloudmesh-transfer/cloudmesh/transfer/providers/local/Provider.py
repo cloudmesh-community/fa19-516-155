c6bc050043008612f3fdac7c06061b99fd11d375
KetanP <kpimparkar@gmail.com>
2019-11-04 23:25:37 -0700

    Added list and delete methods in local provider.

--- cloudmesh-transfer/cloudmesh/transfer/providers/local/Provider.py
+++ cloudmesh-transfer/cloudmesh/transfer/providers/local/Provider.py
@@ -14,6 +14,9 @@ from cloudmesh.common.Printer import Printer
 from cloudmesh.configuration.Config import Config
 
 from pathlib import Path
+from glob import glob
+import os
+import shutil
 
 
 class Provider(StorageABC):
@@ -52,91 +55,196 @@ class Provider(StorageABC):
             print(kwargs.get('sourceObj'))
             print(kwargs.get('target'))
             print(kwargs.get('targetObj'))
+            print(self.credentials)
 
-        self.targetCSP = kwargs.get('target')
+    # Processing --source/service and --target arguments separately.
+    # This is a provider class for local storage hence --source/service will \
+    # always be "local"
+
+        self.sourceCSP = self.service
+
         try:
             self.config = Config(config_path=config)
-            self.yaml_content = self.config["cloudmesh.storage." \
-                                            f"{self.targetCSP}"]
+            self.yaml_content_source = self.config["cloudmesh.storage." 
+                                                   f"{self.sourceCSP}"]
+            self.source_kind = self.yaml_content_source["cm"]["kind"]
+            self.source_credentials = self.yaml_content_source["credentials"]
+
+            print("Accessing local storage location.")
+            if kwargs.get('sourceObj'):
+                self.local_location = Path(self.yaml_content_source['default'][
+                                      'directory'], kwargs.get('sourceObj'))
+            else:
+                self.local_location = self.yaml_content_source['default'][
+                    'directory']
+
+            print("===> ", type(self.local_location),
+                  self.local_location.is_file(),
+                  os.path.isfile(self.local_location.parts[-1]),
+                  self.local_location.expanduser().is_file())
+
+            if kwargs.get("debug"):
+                print(f"\nLocal location to access {self.local_location}")
+
+
+            if kwargs.get('target'):
+                self.targetCSP = kwargs.get('target')
+                self.yaml_content_target = self.config["cloudmesh.storage." 
+                                                       f"{self.targetCSP}"]
+                self.target_kind = self.yaml_content_target["cm"]["kind"]
+                self.target_credentials = self.yaml_content_target[
+                                                            "credentials"]
         except Exception as e:
             Console.error(f"Couldn't access cloudmesh.yaml. Error - {e}")
             return ()
 
         if kwargs.get("debug"):
-            pprint(self.yaml_content)
+            VERBOSE(self.yaml_content_source)
+            if kwargs.get('target'):
+                VERBOSE(self.yaml_content_target)
 
-        self.kind = self.yaml_content["cm"]["kind"]
-        self.credentials = self.yaml_content["credentials"]
+        banner(f"Source CSP: {self.source_kind}")
+        if kwargs.get('target'):
+            banner(f"Target CSP: {self.target_kind}")
 
-        banner(f"Working on {self.kind} cloud service.")
+        # Creating connection with the target CSP. This done only if the
+        # --target argument is provided. Only "copy" command is expected to
+        # have --target argument.
 
-        if self.kind == "awss3":
-            print("Create AWS connection.")
+        if kwargs.get('target'):
+            if self.target_kind == "awss3":
+                print("Create AWS connection.")
 
-            if 'TBD' == self.credentials["access_key_id"] or \
-                    'TBD' == self.credentials["secret_access_key"] or \
-                    'TBD' == self.credentials["region"]:
-                Console.error("Critical details missing from .yaml file. TBD "
-                              "not allowed. Please check.")
+                if 'TBD' == self.credentials["access_key_id"] or \
+                        'TBD' == self.credentials["secret_access_key"] or \
+                        'TBD' == self.credentials["region"]:
+                    Console.error("Critical details missing from .yaml file. "
+                                  "TBD  not allowed. Please check.")
 
-            try:
-                self.s3_client = boto3.client(
-                    's3',
-                    aws_access_key_id=self.credentials["access_key_id"],
-                    aws_secret_access_key=self.credentials["secret_access_key"],
-                    region_name=self.credentials["region"]
-                )
-                Console.ok(f"Successful connection to {self.kind} is made.")
-            except ClientError as e:
-                Console.error(e, prefix=True, traceflag=True)
+                try:
+                    self.s3_client = boto3.client(
+                        's3',
+                        aws_access_key_id=self.credentials["access_key_id"],
+                        aws_secret_access_key=self.credentials[
+                            "secret_access_key"],
+                        region_name=self.credentials["region"]
+                    )
+                    Console.ok(f"Successful connection to {self.kind} is made.")
+                except ClientError as e:
+                    Console.error(e, prefix=True, traceflag=True)
 
-        elif self.kind == "azureblob":
-            print("Create Azure connection.")
-            raise NotImplementedError
-            return {}
-
-        elif self.kind == "local":
-            print("Accessing local storage location.")
-            if kwargs.get('targetObj'):
-                self.local_location = Path(self.yaml_content['default'][
-                                               'directory'],
-                                           kwargs.get('targetObj'))
+            elif self.kind == "azureblob":
+                print("Create Azure connection.")
+                raise NotImplementedError
+                return {}
             else:
-                self.local_location = self.yaml_content['default'][
-                    'directory']
+                raise NotImplementedError
+                return {}
 
-            if kwargs.get("debug"):
-                print(f"\nLocal location to access {self.local_location}")
+# TODO - check hor to pass recursive argument from master provider & transfer.py
 
-        else:
-            raise NotImplementedError
-            return {}
-
-    def list(self, service=None, target=None, recursive=False):
+    def list(self, service=None, sourceObj=None, recursive=False):
         """
         Method to enlist all objects of target location.
 
         :param service: local/aws/azure
-        :param target: target directory or file
+        :param source: target directory or file
         :param recursive: Boolean to indicate if sub components to be enlisted
         :return: list of lists containing objects from target location
         """
-        if self.kind == "awss3":
-            pass
-        elif self.kind == "azureblob":
-            pass
-        elif self.kind == "local":
-            pass
+        if self.source_kind == "awss3":
+            Console.error("This command should flow to AWS provider. Please "
+                          "check.")
+            return
+        elif self.source_kind == "azureblob":
+            Console.error("This command should flow to AWS provider. Please "
+                          "check.")
+            return
+        elif self.source_kind == "local":
+            banner(f"Executing list method for local storage:\nSource object "
+                   f"is {self.local_location}")
+            if self.local_location.exists():
+                if self.local_location.expanduser().is_file():
+                    os.chdir(os.path.split(self.local_location.expanduser())[0])
+
+                    if len(glob(sourceObj)) > 0:
+                        Console.ok("List of file(s):\n"
+                                   f"{self.local_location.expanduser()}")
+                    else:
+                        Console.error(f"File not found "
+                                      f"{self.local_location.expanduser()}")
+                elif self.local_location.expanduser().is_dir():
+                    os.chdir(self.local_location.expanduser())
+                    Console.ok(f"List if files in {self.local_location}:\n")
+                    for f in glob("**", recursive=recursive):
+                        print(Path.cwd() / f)
+            else:
+                Console.error(f"Source object {self.local_location} does not "
+                              f"exist.")
         else:
             raise NotImplementedError
             return {}
 
+    def delete(self, service="local", sourceObj=None, recursive=False):
+        """
+        This method deletes file(s) / folder(s) from the source location.
+
+        :param service: "local" for this provider
+        :param sourceObj: A file or folder to delete
+        :param recursive: Delete files from folder/subfolders
+        :return: None
+        """
+        if self.source_kind == "awss3":
+            Console.error("This command should flow to AWS provider. Please "
+                          "check.")
+            return
+        elif self.source_kind == "azureblob":
+            Console.error("This command should flow to AWS provider. Please "
+                          "check.")
+            return
+        elif self.source_kind == "local":
+            banner(f"Executing delete method for local storage:\nSource object "
+                   f"is {self.local_location}")
+
+            if self.local_location.exists():
+                if self.local_location.expanduser().is_file():
+                    os.chdir(os.path.split(self.local_location.expanduser())[0])
+
+                    if len(glob(sourceObj)) > 0:
+                        Console.ok("Following file will be removed:\n"
+                                   f"{self.local_location.expanduser()}")
+                        os.remove(self.local_location.expanduser())
+                    else:
+                        Console.error(f"File not found "
+                                      f"{self.local_location.expanduser()}")
+                elif self.local_location.expanduser().is_dir():
+                    os.chdir(self.local_location.expanduser())
+                    Console.ok(f"Following objects will be removed from: "
+                               f"{self.local_location}:\n")
+                    for f in glob("**", recursive=recursive):
+                        print(Path.cwd() / f)
+
+                    shutil.rmtree(self.local_location.expanduser())
+            else:
+                Console.error(f"Source object {self.local_location} does not exist.")
+        else:
+            raise NotImplementedError
+            return {}
+
+
 def main():
     print("Instantiating")
-    instance = Provider(service="local", sourceObj="abcd.txt", target="aws",
+    # following instantiating for copy command
+    # instance = Provider(service="local", sourceObj="abcd.txt", target="aws",
+    #                     targetObj=None, debug=True)
+
+    # Instantiating for list/delete command
+    instance = Provider(service="local", sourceObj="a",
                         targetObj=None, debug=True)
 
-    instance.list(service="local", target=None)
+    # instance.list(service="local", sourceObj="a", recursive=True)
+
+    instance.delete(service="local", sourceObj="a", recursive=True)
 
 if __name__ == "__main__":
     main()
