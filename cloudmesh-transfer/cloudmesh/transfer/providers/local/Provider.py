ed129c9aa441a7c58d68df375a1139780d2b9566
KetanP <kpimparkar@gmail.com>
2019-11-13 23:45:28 -0700

    Added local to S3 transfer.

--- cloudmesh-transfer/cloudmesh/transfer/providers/local/Provider.py
+++ cloudmesh-transfer/cloudmesh/transfer/providers/local/Provider.py
@@ -57,15 +57,15 @@ class Provider(StorageABC):
             print(kwargs.get('targetObj'))
             print(self.credentials)
 
-    # Processing --source/service and --target arguments separately.
-    # This is a provider class for local storage hence --source/service will \
-    # always be "local"
+        # Processing --source/service and --target arguments separately.
+        # This is a provider class for local storage hence --source/service will \
+        # always be "local"
 
         self.sourceCSP = self.service
 
         try:
             self.config = Config(config_path=config)
-            self.yaml_content_source = self.config["cloudmesh.storage." 
+            self.yaml_content_source = self.config["cloudmesh.storage."
                                                    f"{self.sourceCSP}"]
             self.source_kind = self.yaml_content_source["cm"]["kind"]
             self.source_credentials = self.yaml_content_source["credentials"]
@@ -73,7 +73,8 @@ class Provider(StorageABC):
             print("Accessing local storage location.")
             if kwargs.get('sourceObj'):
                 self.local_location = Path(self.yaml_content_source['default'][
-                                      'directory'], kwargs.get('sourceObj'))
+                                               'directory'],
+                                           kwargs.get('sourceObj'))
             else:
                 self.local_location = self.yaml_content_source['default'][
                     'directory']
@@ -81,14 +82,13 @@ class Provider(StorageABC):
             if kwargs.get("debug"):
                 print(f"\nLocal location to access {self.local_location}")
 
-
             if kwargs.get('target'):
                 self.targetCSP = kwargs.get('target')
-                self.yaml_content_target = self.config["cloudmesh.storage." 
+                self.yaml_content_target = self.config["cloudmesh.storage."
                                                        f"{self.targetCSP}"]
                 self.target_kind = self.yaml_content_target["cm"]["kind"]
                 self.target_credentials = self.yaml_content_target[
-                                                            "credentials"]
+                    "credentials"]
                 self.target_container = self.target_credentials["container"]
 
         except Exception as e:
@@ -112,10 +112,9 @@ class Provider(StorageABC):
             if self.target_kind == "awss3":
                 print("Create AWS connection.")
 
-                if 'TBD' == self.target_credentials["access_key_id"] or \
-                        'TBD' == self.target_credentials["secret_access_key"]\
-                        or \
-                        'TBD' == self.target_credentials["region"]:
+                if 'TBD' == self.target_credentials["access_key_id"] \
+                        or 'TBD' == self.target_credentials["secret_access_key"] \
+                        or 'TBD' == self.target_credentials["region"]:
                     Console.error("Critical details missing from .yaml file. "
                                   "TBD  not allowed. Please check.")
 
@@ -128,20 +127,19 @@ class Provider(StorageABC):
                             "secret_access_key"],
                         region_name=self.target_credentials["region"]
                     )
-                    Console.ok(f"Successful connection to {self.target_kind} is "
-                               f"made.")
+                    Console.ok(
+                        f"Successful connection to {self.target_kind} is "
+                        f"made.")
                 except ClientError as e:
                     Console.error(e, prefix=True, traceflag=True)
 
             elif self.kind == "azureblob":
                 print("Create Azure connection.")
                 raise NotImplementedError
-                return {}
             else:
                 raise NotImplementedError
-                return {}
 
-# TODO - check hor to pass recursive argument from master provider & transfer.py
+    # TODO - check hor to pass recursive argument from master provider & transfer.py
 
     def list(self, service=None, sourceObj=None, recursive=False):
         """
@@ -226,11 +224,11 @@ class Provider(StorageABC):
 
                     shutil.rmtree(self.local_location.expanduser())
             else:
-                Console.error(f"Source object {self.local_location} does not exist.")
+                Console.error(
+                    f"Source object {self.local_location} does not exist.")
         else:
             raise NotImplementedError
             return {}
-
 
     def s3_bucket_exists(self, target_container):
         """
@@ -246,9 +244,8 @@ class Provider(StorageABC):
             return False
         return True
 
-
     def copy(self, service="local", sourceObj="abcd.txt", target="aws",
-                                    targetObj=None, debug=True):
+             targetObj=None, debug=True):
         """
         copy method copies files/directories from local storage to target CSP
 
@@ -258,22 +255,83 @@ class Provider(StorageABC):
         :param debug: Boolean indicating debug mode
         :return: None
         """
+        # To copy the whole cmStorage directory, pls provide sourceObj=None
+
         if self.s3_bucket_exists(self.target_container):
             Console.ok(f"AWS S3 bucket {self.target_container} exists.")
 
+            # TODO : Check CLI option
+            # CLI option
+            # aws s3 cp C:\Users\kpimp\cmStorage
+            # s3://bucket-iris.json/cmStorage --recursive
 
+            if sourceObj:
+                self.local_location = Path(self.yaml_content_source['default'][
+                                               'directory'], sourceObj)
+                print("=====> local location ", self.local_location)
+            else:
+                sourceObj = "cmStorage"
+                self.local_location = self.yaml_content_source['default'][
+                    'directory']
+
+            if targetObj is None:
+                targetObj = sourceObj
+
+            source_path = Path(self.local_location)
+
+            try:
+                if source_path.expanduser().is_file():
+                    # TODO: Use queue here
+                    print(f"Copying file. Pushed {source_path} to the queue.")
+                    os.chdir(os.path.split(self.local_location.expanduser())[0])
+                    print("chdir to ", os.getcwd())
+                    try:
+                        response = self.s3_client.upload_file(sourceObj,
+                                                              self.target_container,
+                                                              targetObj)
+                        Console.ok(f"Uploaded: {sourceObj}")
+                    except ClientError as e:
+                        Console.error(f"Error while uploading {source_path} "
+                                      f"to S3 bucket: \n", e)
+                elif source_path.expanduser().is_dir():
+                    print("Copying directory recursively")
+                    os.chdir(source_path.expanduser())
+                    print("chdir to ", os.getcwd())
+
+                    for file in glob('**', recursive=True):
+                        # TODO: This creates files as foldername/filename
+                        # Check how to create directory structure in S3
+
+                        print(file)
+                        if Path(file).is_file():
+                            # TODO: Use queue here
+                            print(f"Pushed {Path(file)} to the queue {file}.")
+                            targetObj = file
+                            try:
+                                response = self.s3_client.upload_file(file,
+                                                                      self.target_container,
+                                                                      targetObj)
+                                Console.ok(f"Uploaded: {file}")
+                            except ClientError as e:
+                                Console.error(
+                                    f"Error while uploading {source_path} "
+                                    f"to S3 bucket: \n", e)
+                else:
+                    Console.error(f"Invalid source object type: {source_path}")
+                    return
+            except Exception as e:
+                print(e)
 
         else:
             Console.error(f"AWS S3 bucket {self.target_container} does not "
                           f"exist.")
 
 
-
 def main():
     print("Instantiating")
     # following instantiating for copy command
     instance = Provider(service="local", sourceObj="abcd.txt", target="aws",
-                         targetObj=None, debug=True)
+                        targetObj=None, debug=True)
 
     # Instantiating for list/delete command
     # instance = Provider(service="local", sourceObj="a",
@@ -283,8 +341,9 @@ def main():
 
     # instance.delete(service="local", sourceObj="a", recursive=True)
 
-    instance.copy(service="local", sourceObj="abcd.txt", target="aws",
-                            targetObj=None, debug=True)
+    instance.copy(service="local", sourceObj='abcd.txt', target="aws",
+                  targetObj=None, debug=True)
+
 
 if __name__ == "__main__":
     main()
